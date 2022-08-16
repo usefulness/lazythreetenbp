2aaeaecd980e492d17e544f7d493f0c78fc352b4
mateuszkwiecinski <mateuszkwiecinski@users.noreply.github.com>
2022-08-16 17:20:44 +0000

    Update TZDB to 2022c

--- lazythreetenbp/src/main/kotlin/com/usefulness/threetenbp/GeneratedZoneIdsProvider.kt
+++ lazythreetenbp/src/main/kotlin/com/usefulness/threetenbp/GeneratedZoneIdsProvider.kt
@@ -5,7 +5,7 @@ import kotlin.String
 import kotlin.collections.Collection
 
 internal object GeneratedZoneIdsProvider : ZoneIdsProvider {
-    private const val VERSION_ID: String = "2022b"
+    private const val VERSION_ID: String = "2022c"
 
     private val ZONE_IDS: Collection<String> = setOf(
         "Africa/Abidjan",
